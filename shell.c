51eedbf07fef8cf8865fb5c6383f6261251c5a93
Dexter Gramfors <dexter.gramfors@gmail.com>
2015-04-16 10:51:15 +0200

    Use HOME evironment variable.

--- shell.c
+++ shell.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "cd.h"
 void get_command();
@@ -6,9 +7,12 @@ void handle_command();
 int starts_with(char*, char*);
 void substring_to_end(char* result, char* string, size_t start);
 void substring(char* result, char* string, size_t start, size_t end);
+void handle_cd(char* command);
+char* home;
 
 int main()
 {
+	home = getenv("HOME");
 	get_command();
 	return 0;
 }
@@ -16,6 +20,7 @@ int main()
 void get_command() {
 	char command[80];
 	char* successful_read;
+	printf("%s\n", home);
 
 	while(1) {
 		print_working_directory();
@@ -29,12 +34,20 @@ void get_command() {
 }
 
 void handle_command(char* command) {
-	char path[80];
 	if (starts_with(command, "cd")) {
-		substring_to_end(path, command, 3);
-		change_working_directory(path);
+		handle_cd(command);
 	} else {
 		printf("Unknown command '%s'\n", command);
+	}
+}
+
+void handle_cd(char* command) {
+	char path[80];
+	if (strcmp(command, "cd") == 0) {
+		change_working_directory(home);
+	} else {
+		substring_to_end(path, command, 3);
+		change_working_directory(path);
 	}
 }
 
